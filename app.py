b9fcd26f571170397744a0c6d28d20b8a31e64b0
Xian <joeyang0314@gmail.com>
2018-08-20 15:06:38 +0800

    csv test

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def handle_message(event):
 
 import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8888))
     app.run(host='0.0.0.0', port=port)
