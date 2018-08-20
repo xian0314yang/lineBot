f1e61031c5ee782bd726056985b076395dd98434
Xian <joeyang0314@gmail.com>
2018-08-20 16:31:48 +0800

    csv test

--- app.py
+++ app.py
@@ -36,8 +36,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     #message = TextSendMessage(text=event.message.text)
-	message = TextSendMessage(text='hello world')
-    line_bot_api.reply_message(event.reply_token, message)
+
+    #line_bot_api.reply_message(event.reply_token, message)
+	
+	message = event.message.text
+    if 'Hello' in message:
+        reply_message = 'World'
+    elif '您好' in message:
+        reply_message = '嗨'
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
+
 
 import os
 if __name__ == "__main__":
