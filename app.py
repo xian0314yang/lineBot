42e34663b6a4921be27801f0037d773cc796df4a
Xian <joeyang0314@gmail.com>
2018-08-20 17:10:12 +0800

    csv test

--- app.py
+++ app.py
@@ -46,10 +46,10 @@ def handle_message(event):
         message = 'World'
     elif ('您好' in message):
         message = '嗨'
-	else
+    else
         message = '幹你娘'
 #    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
-	line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(event.reply_token, message)
 
 #import os
 if __name__ == "__main__":
