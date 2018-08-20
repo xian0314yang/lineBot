9effa3748b8c3b19ace07d4b035f0c37d1171d6c
Xian <joeyang0314@gmail.com>
2018-08-20 22:04:19 +0800

    csv test

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def handle_message(event):
                 s1 = x	
                 break	
 
-        s1 = s1.split(',', 1 )     	
+        s1 = s1.split(',')[1]    	
 		
 				
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=s1))
