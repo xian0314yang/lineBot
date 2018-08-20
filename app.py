b4bbb8dfd9caa805cdc4595861251090853bff7f
Xian <joeyang0314@gmail.com>
2018-08-20 16:34:51 +0800

    csv test

--- app.py
+++ app.py
@@ -40,10 +40,10 @@ def handle_message(event):
     #line_bot_api.reply_message(event.reply_token, message)
 	
 	message = event.message.text
-    if 'Hello' in message:
-        reply_message = 'World'
-    elif '您好' in message:
-        reply_message = '嗨'
+    if ("Hello") in message:
+        reply_message = ("World")
+    elif ('您好') in message:
+        reply_message = ('嗨')
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
 
 
