8b316ffcebacdea12ca9a4bfaba539fd106a38ab
Xian <joeyang0314@gmail.com>
2018-08-21 18:40:25 +0800

    new bot function

--- app.py
+++ app.py
@@ -511,7 +511,8 @@ def handle_message(event):
         buttons_template = ''.join(random.choice(string.digits) for x in range(3))	
         line_bot_api.reply_message(event.reply_token, buttons_template)	
         return 0
-		
+	
+    message = event.message.text	
     lines = [line.strip() for line in open('data.csv')]
     for x in lines:
         match = re.search(message,x)
