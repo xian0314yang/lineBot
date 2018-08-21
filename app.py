23af5de1650be633e3461a6bc18881738c353808
Xian <joeyang0314@gmail.com>
2018-08-21 20:13:14 +0800

    new bot function

--- app.py
+++ app.py
@@ -303,7 +303,7 @@ def handle_message(event):
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text=message))	
-            break	
+            #break	
         elif event.message.text == "eyny":
             content = eyny_movie()
             line_bot_api.reply_message(
