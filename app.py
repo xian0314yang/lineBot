a419bd42da16c0dabb60c62accbf5106cf0f85ed
Xian <joeyang0314@gmail.com>
2018-08-23 20:07:04 +0800

    add chinese reply

--- app.py
+++ app.py
@@ -552,13 +552,13 @@ def handle_message(event):
 			
             line_bot_api.reply_message(event.reply_token, buttons_template)
         
-        else:
-            num = random.randint(1,20)
-            message = ''.join(chr(random.randint(0x4e00, 0x9fa5)) for x in range(num))
+#        else:
+#            num = random.randint(1,20)
+#            message = ''.join(chr(random.randint(0x4e00, 0x9fa5)) for x in range(num))
 #            message = ''.join(chr(random.randint(0x0000, 0xffff)) for x in range(num))			
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=message))				
+#            line_bot_api.reply_message(
+#                event.reply_token,
+#                TextSendMessage(text=message))				
 
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_sticker_message(event):
